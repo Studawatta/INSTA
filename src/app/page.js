46548e80337fc198aa92d8270d481f7693ab6175
Sandun Thisara <89610546+Studawatta@users.noreply.github.com>
2024-06-04 20:37:33 +0530

    header completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const Home = () => {
-  return (
-    <div className='text-red-300 '>Home</div>
-  )
+  return <div className="text-red-400">Home</div>
 }
 
 export default Home
---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+        {/* logo */}
+        <Link href={'/'} className="hidden lg:inline-flex">
+          <Image
+            src={'/insta_black.webp'}
+            alt="instagram icon"
+            width={96}
+            height={96}
+          />
+        </Link>
+        <Link href={'/'} className=" lg:hidden">
+          <Image
+            src={'/insta.webp'}
+            alt="instagram icon"
+            width={96}
+            height={96}
+          />
+        </Link>
+
+        {/* search input */}
+
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px] "
+        />
+
+        {/* menu items */}
+
+        <button className="text-sm font-semibold text-blue-500">Log In</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
